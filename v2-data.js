8c26bb2f116829a40b94be9600119111cec794c9
yeongtaek, Hong <mozodev@users.noreply.github.com>
2017-08-21 20:06:36 +0900

    json을 조금 보기 좋게 바꿔 봅니다.

--- v2-data.js
+++ v2-data.js
@@ -1,30 +1,58 @@
-let catData={"categories":[
-    {"category":"Deviation", "colour":"#E6B00F",
-    "description":"Emphasise variations (+/-) from a fixed reference point. Typically the reference point is zero but it can also be a target or a long-term average. Can also be used to show sentiment (positive/neutral/negative)",
-    "example":"Trade surplus/deficit, climate change"},
-    {"category":"Correlation", "colour":"#977348",
-    "description":"Show the relationship between two or more variables. Be mindful that, unless you tell them otherwise, many readers will assume the relationships you show them to be causal (i.e. one causes the other)",
-    "example":"Inflation & unemployment, income & life expectancy"},
-{"category":"시간에 의한 변화", "colour":"#28903B",
-	"description":"변화의 경향을 잘 보여준다. 짧은 구간(하루)이거나 수십년이나 세기에 걸쳐 잇달을 수도 있다: 독자에게 적확한 맥락을 전달하기 위해서 정확한 구간길이를 선택하는 것이 중요하다.",
-	"example":"주가 변동, economic time series"},
-{"category":"Ranking", "colour":"#008EB0",
-    "description":"Use where an item’s position in an ordered list is more important than its absolute or relative value. Don’t be afraid to highlight the points of interest.",
-    "example":"Wealth, deprivation, league tables, constituency election results"},
-{"category":"Distribution", "colour":"#F26522",
-	"description":"Show values in a dataset and how often they occur. The shape (or ‘skew’) of a distribution can be a memorable way of highlighting the lack of uniformity or equality in the data",
-	"example":"Income distribution, population (age/sex) distribution"},
-{"category":"Part to whole", "colour":"#0054A6",
-    "description":"Show how a single entity can be broken down into its component elements. If the reader’s interest is solely in the size of the components, consider a magnitude-type chart instead",
-    "example":"Fiscal budgets, company structures, national election results"},
-{"category":"Magnitude", "colour":"#A3488C",
-"description":"Show size comparisons. These can be relative (just being able to see larger/bigger) or absolute (need to see fine differences). Usually these show a ‘counted’ number (for example, barrels, dollars or people) rather than a calculated rate or per cent",
-"example":"Commodity production, market capitalisation"},
-{"category":"Spatial", "colour":"#9C9D94",
-"description":"Used only when precise locations or geographical patterns in data are more important to the reader than anything else. ",
-"example":"Locator maps, population density, natural resource locations, natural disaster risk/impact, catchment areas, variation in election results"},
-{"category":"Flow", "colour":"#ABBD67",
-"description":"Show the reader volumes or intensity of movement between two or more states or conditions. These might be logical sequences or geographical locations",
-"example":"Movement of funds, trade, migrants, lawsuits, information; relationship graphs"}]
+let catData = {
+  "categories": [
+    {
+      "category": "Deviation",
+      "colour": "#E6B00F",
+      "description": "Emphasise variations (+/-) from a fixed reference point. Typically the reference point is zero but it can also be a target or a long-term average. Can also be used to show sentiment (positive/neutral/negative)",
+      "example": "Trade surplus/deficit, climate change"
+    },
+    {
+      "category": "Correlation",
+      "colour": "#977348",
+      "description": "Show the relationship between two or more variables. Be mindful that, unless you tell them otherwise, many readers will assume the relationships you show them to be causal (i.e. one causes the other)",
+      "example": "Inflation & unemployment, income & life expectancy"
+    },
+    {
+      "category": "시간에 의한 변화",
+      "colour": "#28903B",
+      "description": "변화의 경향을 잘 보여준다. 짧은 구간(하루)이거나 수십년이나 세기에 걸쳐 잇달을 수도 있다: 독자에게 적확한 맥락을 전달하기 위해서 정확한 구간길이를 선택하는 것이 중요하다.",
+      "example": "주가 변동, economic time series"
+    },
+    {
+      "category": "Ranking",
+      "colour": "#008EB0",
+      "description": "Use where an item’s position in an ordered list is more important than its absolute or relative value. Don’t be afraid to highlight the points of interest.",
+      "example": "Wealth, deprivation, league tables, constituency election results"
+    },
+    {
+      "category": "Distribution",
+      "colour": "#F26522",
+      "description": "Show values in a dataset and how often they occur. The shape (or ‘skew’) of a distribution can be a memorable way of highlighting the lack of uniformity or equality in the data",
+      "example": "Income distribution, population (age/sex) distribution"
+    },
+    {
+      "category": "Part to whole",
+      "colour": "#0054A6",
+      "description": "Show how a single entity can be broken down into its component elements. If the reader’s interest is solely in the size of the components, consider a magnitude-type chart instead",
+      "example": "Fiscal budgets, company structures, national election results"
+    },
+    {
+      "category": "Magnitude",
+      "colour": "#A3488C",
+      "description": "Show size comparisons. These can be relative (just being able to see larger/bigger) or absolute (need to see fine differences). Usually these show a ‘counted’ number (for example, barrels, dollars or people) rather than a calculated rate or per cent",
+      "example": "Commodity production, market capitalisation"
+    },
+    {
+      "category": "Spatial",
+      "colour": "#9C9D94",
+      "description": "Used only when precise locations or geographical patterns in data are more important to the reader than anything else. ",
+      "example": "Locator maps, population density, natural resource locations, natural disaster risk/impact, catchment areas, variation in election results"
+    },
+    {
+      "category": "Flow",
+      "colour": "#ABBD67",
+      "description": "Show the reader volumes or intensity of movement between two or more states or conditions. These might be logical sequences or geographical locations",
+      "example": "Movement of funds, trade, migrants, lawsuits, information; relationship graphs"
+    }
+  ]
 }
-
